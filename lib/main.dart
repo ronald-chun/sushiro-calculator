3aba39346356f01ca8f3401f232b1023ad1a342b
Ronald Wong <ronaldchunyin@gmail.com>
2024-08-31 22:58:40 +0800

    Update gitlab-ci.yml

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
@@ -17,7 +17,7 @@ void main() async {
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
-  await dotenv.load(fileName: '.env');
+  // await dotenv.load(fileName: '.env');
   runApp(MyApp(settingsController: settingsController));
 }
 
